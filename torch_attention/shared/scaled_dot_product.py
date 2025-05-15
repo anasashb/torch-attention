8faaf4a7a378362509878d0073c3e4ba72146484
anasashb <ba.anasashvili@gmail.com>
2025-05-15 22:07:30 +0400

    Begin vanila attn dev

---
+++ torch_attention/shared/scaled_dot_product.py
@@ -0,0 +1,51 @@
+from math import sqrt
+from typing import Optional
+
+import numpy as np
+import torch
+from torch import Tensor
+
+from torch_attention.shared._attention_base import AttentionBase
+
+
+class ScaledDotProductAttention(AttentionBase):
+
+    def _attend(
+        self,
+        query: Tensor,
+        key: Tensor,
+        value: Tensor,
+        scale_factor: float,
+        mask: Optional[Tensor],
+    ):
+
+        # Short-hand notation for shapes
+        Bq, Hq, Lq, _ = query.shape
+        _, _, Lk, _ = key.shape
+
+        # TODO fix shape misalignment
+        # Get raw scores
+        scores = torch.einsum("blhe,bshe->bhls", query, key)
+
+        # Apply mask if needed
+        if self.use_mask:
+            if mask is None:
+                mask = torch.triu(
+                    torch.ones(Lq, Lk, dtype=torch.bool, device=query.device),
+                    diagonal=1,
+                )
+                mask = mask.unsqueeze(0).unsqueeze(0).expand(Bq, Hq, Lq, Lk)
+
+            scores.masked_fill_(mask, float("-inf"))
+
+        # Get attention scores
+        attn_weights = self.dropout(
+            torch.softmax(scale_factor * scores, dim=-1)
+        )
+
+        # TODO if dropout handling somewhere here
+
+        # Get attention outputs
+        attn_outputs = torch.einsum("bhls,bshd->blhd", attn_weights, value)
+
+        return attn_outputs, attn_weights
