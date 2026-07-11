5151f5b014c184ebd5a1dbd541a72026929e2273
anasashb <ba.anasashvili@gmail.com>
2026-07-11 21:23:38 +0400

    feat: export scaled dot-product attention to init

--- src/torch_attention/__init__.py
+++ src/torch_attention/__init__.py
@@ -1,5 +1,9 @@
 from importlib.metadata import PackageNotFoundError, version
 
+from torch_attention.shared import ScaledDotProductAttention
+
+__all__ = ["ScaledDotProductAttention", "__version__"]
+
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:
