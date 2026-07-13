7d1ac0dc39a01462e396346eb3e033a4dbf1a528
anasashb <ba.anasashvili@gmail.com>
2026-07-13 18:18:03 +0400

    chore: initial package rename

--- src/torch_attention/__init__.py
+++ src/torch_attention/__init__.py
@@ -5,6 +5,6 @@ from torch_attention.shared import ScaledDotProductAttention
 __all__ = ["ScaledDotProductAttention", "__version__"]
 
 try:
-    __version__ = version("torch-attention")
+    __version__ = version("adlers")
 except PackageNotFoundError:
     __version__ = "0.0.0"
