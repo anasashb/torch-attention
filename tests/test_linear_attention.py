2449dc1527b327e50fcb463cd000bfebf6018a15
anasashb <ba.anasashvili@gmail.com>
2026-09-04 01:03:34 +0400

    test: require LinearAttention tuple output

--- tests/test_linear_attention.py
+++ tests/test_linear_attention.py
@@ -11,7 +11,7 @@ def test_linear_attention_matches_pinned_fast_transformers_behavior() -> None:
     value = torch.tensor([[[[1.0, 2.0], [3.0, 4.0]]]]).repeat(1, 3, 1, 1)
     attention = LinearAttention(eps=1e-6)
 
-    output = attention(
+    output, attn_weights = attention(
         query=query,
         key=key,
         value=value,
@@ -21,6 +21,7 @@ def test_linear_attention_matches_pinned_fast_transformers_behavior() -> None:
     expected_output = torch.tensor(
         [[[[1.77024138, 2.77024126], [2.22975802, 3.22975779]]]]
     ).repeat(1, 3, 1, 1)
+    assert attn_weights is None
     torch.testing.assert_close(output, expected_output)
 
 
@@ -31,7 +32,7 @@ def test_linear_attention_accepts_tensor_feature_map() -> None:
     value = torch.tensor([[[[2.0], [4.0]]]])
     attention = LinearAttention(feature_map=torch.ones_like, eps=0.0)
 
-    output = attention(
+    output, _ = attention(
         query=query,
         key=key,
         value=value,
@@ -49,7 +50,7 @@ def test_linear_attention_applies_key_padding_mask() -> None:
     attn_mask = torch.tensor([[[[False, True]]]])
     attention = LinearAttention(feature_map=torch.ones_like, eps=0.0)
 
-    output = attention(
+    output, _ = attention(
         query=query,
         key=key,
         value=value,
