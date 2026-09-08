abf1755abac02787ab4b58e8b44242ff20e23179
anasashb <ba.anasashvili@gmail.com>
2026-09-09 00:35:33 +0400

    sort causal product wrapper imports

--- src/adlers/nlp/causal_linear_attention.py
+++ src/adlers/nlp/causal_linear_attention.py
@@ -17,15 +17,13 @@
 import torch
 from torch.nn import Module
 
+from .causal_product_cpu import causal_dot_backward as causal_dot_backward_cpu
+from .causal_product_cpu import causal_dot_product as causal_dot_product_cpu
 from .linear_attention import _elu_feature_map
 
-from .causal_product_cpu import causal_dot_product as causal_dot_product_cpu, \
-    causal_dot_backward as causal_dot_backward_cpu
-
 try:
-    from .causal_product_cuda import \
-        causal_dot_product as causal_dot_product_cuda, \
-        causal_dot_backward as causal_dot_backward_cuda
+    from .causal_product_cuda import causal_dot_backward as causal_dot_backward_cuda
+    from .causal_product_cuda import causal_dot_product as causal_dot_product_cuda
 except ImportError:
     causal_dot_product_cuda = causal_dot_backward_cuda = None
 
