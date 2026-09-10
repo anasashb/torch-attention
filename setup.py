d1a5a2ad852175d874e9c352837c5f6aa89dbdaa
anasashb <ba.anasashvili@gmail.com>
2026-09-11 01:28:45 +0400

    build: cuda extension

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 from setuptools import setup
-from torch.utils.cpp_extension import BuildExtension, CppExtension
+from torch.utils.cpp_extension import (
+    CUDA_HOME,
+    BuildExtension,
+    CppExtension,
+    CUDAExtension,
+)
+
+extensions = [
+    CppExtension(
+        name="adlers.nlp.causal_product_cpu",
+        sources=["src/adlers/csrc/causal_product_cpu.cpp"],
+        extra_compile_args=["-fopenmp", "-ffast-math"],
+        extra_link_args=["-fopenmp"],
+    )
+]
+
+if CUDA_HOME is not None:
+    extensions.append(
+        CUDAExtension(
+            name="adlers.nlp.causal_product_cuda",
+            sources=["src/adlers/csrc/cuda/causal_product_cuda.cu"],
+        )
+    )
 
 setup(
-    ext_modules=[
-        CppExtension(
-            name="adlers.nlp.causal_product_cpu",
-            sources=["src/adlers/csrc/causal_product_cpu.cpp"],
-            extra_compile_args=["-fopenmp", "-ffast-math"],
-            extra_link_args=["-fopenmp"],
-        )
-    ],
+    ext_modules=extensions,
     cmdclass={"build_ext": BuildExtension},
 )
